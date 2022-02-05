d8aa38405719e01f0153141ecfa0d6793400e1af
Sythiex <Sythiex11@gmail.com>
2022-02-05 03:22:38 -0600

    /roll updates
    -renamed dice_to_roll to dice
    -inputs that don't specify the number of dice like "d6" are interpreted as "1d6"
    -results will no longer print like an equation if there is no arithmetic to show (avoids situations like "1 = 1")

--- count_bot/cog_misc.py
+++ count_bot/cog_misc.py
@@ -32,37 +32,37 @@ class MiscCommands(commands.Cog, name='Misc Commands'):
 
     @commands.slash_command(guild_ids=guilds)
     async def roll(self, ctx: ApplicationContext,
-                   dice_to_roll: Option(str, "Format is 'XdY' where X is the number of dice to roll and Y is the size of the dice"),
+                   dice: Option(str, "Format is 'XdY' where X is the number of dice to roll and Y is the size of the dice"),
                    modifier: Option(int, "Number added to the result of the roll", required=False, default=0)):
         """Roll some dice 🎲"""
-        print(f'{ctx.author} rolled {dice_to_roll} + {modifier}')
-        # make sure the input contains 'd'
-        dice_to_roll = dice_to_roll.lower()
-        if 'd' not in dice_to_roll:
-            await ctx.interaction.response.send_message(f"'{dice_to_roll}' is not a valid input.", ephemeral=True)
-            return
+        max_number_of_dice = 100
+        max_die_size = 1000000000
+        print(f'{ctx.author} rolled {dice} + {modifier}')
         # split the input and verify there are only 2 parts
-        die_input_split = dice_to_roll.split('d')
+        die_input_split = dice.lower().split('d')
         if len(die_input_split) != 2:
-            await ctx.interaction.response.send_message(f"'{dice_to_roll}' is not a valid input.", ephemeral=True)
+            await ctx.interaction.response.send_message(f"'{dice}' is not a valid input.", ephemeral=True)
             return
         # convert the parts into ints
         try:
             number_of_dice = int(die_input_split[0])
         except ValueError:
-            await ctx.interaction.response.send_message(f"'{die_input_split[0]}' is not an integer.", ephemeral=True)
-            return
+            if die_input_split[0] == '':  # if no number of dice is specified, assume 1
+                number_of_dice = 1
+            else:
+                await ctx.interaction.response.send_message(f"'{die_input_split[0]}' is not an integer.", ephemeral=True)
+                return
         try:
             die_size = int(die_input_split[1])
         except ValueError:
             await ctx.interaction.response.send_message(f"'{die_input_split[1]}' is not an integer.", ephemeral=True)
             return
         # keep numbers "sane"
-        if number_of_dice > 100 or number_of_dice < 1:
-            await ctx.interaction.response.send_message(f"Number of dice must be between 1 and 100, got: {number_of_dice}", ephemeral=True)
+        if number_of_dice > max_number_of_dice or number_of_dice < 1:
+            await ctx.interaction.response.send_message(f"Number of dice must be between 1 and {max_number_of_dice}, got: {number_of_dice}", ephemeral=True)
             return
-        if die_size > 1000000000 or die_size < 1:
-            await ctx.interaction.response.send_message(f"Size of dice must be between 1 and 1000000000, got: {die_size}", ephemeral=True)
+        if die_size > max_die_size or die_size < 1:
+            await ctx.interaction.response.send_message(f"Size of dice must be between 1 and {max_die_size}, got: {die_size}", ephemeral=True)
             return
         # roll all the dice
         total = 0
@@ -73,14 +73,17 @@ class MiscCommands(commands.Cog, name='Misc Commands'):
             result_string += str(this_roll)
             if i != number_of_dice - 1:
                 result_string += ' + '
-        # add the mod
+        # add the modifier
         mod_str = ''
         if modifier != 0:
             total = total + modifier
-            mod_str = f" {'-' if modifier < 0 else '+'} {abs(modifier)}"
+            mod_str = f"{'-' if modifier < 0 else '+'} {abs(modifier)}"
             result_string += f" {'-' if modifier < 0 else '+'} *{abs(modifier)}*"  # uses italics to visually separate the mod from the rolls
-
-        await ctx.interaction.response.send_message(f"Rolled {dice_to_roll}{mod_str}:\n{result_string} = **{total}**")
+        result_string += ' = '
+        # hide result_string if there is no arithmetic to show (to avoid situations like '1 = 1')
+        if number_of_dice == 1 and modifier == 0:
+            result_string = ''
+        await ctx.interaction.response.send_message(f"Rolled {number_of_dice}d{die_size} {mod_str}:\n{result_string}**{total}**")
 
 
 def setup(bot):
